148c0be0720e7ae91a88795503a0cf06ab4a568e
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-03-21 17:31:16 +0500

    factory-method

--- src/main/java/energy_glow/ClassicalMusic.java
+++ src/main/java/energy_glow/ClassicalMusic.java
@@ -2,15 +2,25 @@ package energy_glow;
 
 
 public class ClassicalMusic implements Music{
-//    private String song;
-//    public void doMyInit(){
-//        System.out.println("Initialization of class " + this.getClass() + "...");
-//        song = "Rush E";
-//    }
-//
-//    public void doMyDestroy(){
-//        System.out.println("Distraction of class " + this.getClass() + "...");
-//    }
+    private String song;
+    public void doMyInit(){
+        System.out.println("Initialization of class " + this.getClass() + "...");
+        song = "Rush E";
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Distraction of class " + this.getClass() + "...");
+    }
+    private static ClassicalMusic instance;
+    private ClassicalMusic(){
+
+    }
+    public static ClassicalMusic getClassicalMusicInstance(){
+        if(instance == null){
+            instance = new ClassicalMusic();
+        }
+        return instance;
+    }
     @Override
     public String getSong() {
         //return song;
